a473b754eb9cb417569a361dd2750b2f5f6f773c
s0hum <sohum.p.gupta@gmail.com>
2024-11-18 17:12:48 -0800

    added new 3 spec auto pathing test + Meep Meep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeep2.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeep2.java
@@ -1,6 +1,10 @@
 package com.example.meepmeeptesting;
 
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -8,18 +12,79 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeep2 {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(700);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(90, 80, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(130, 80, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-10, -61.5, Math.toRadians(270)))
-                        .strafeTo(new Vector2d(-10,-50))
-                        .splineTo(new Vector2d(-58,-58), Math.toRadians(225))
+        Pose2d beginPose = new Pose2d(13, -61.5, Math.toRadians(270));
 
-                .build());
+        TrajectoryActionBuilder moveAwayFromBarrier = myBot.getDrive().actionBuilder(beginPose)
+                .strafeTo(new Vector2d(13, -50))
+                .waitSeconds(0.001);
+        TrajectoryActionBuilder moveIntoSpec1Position = moveAwayFromBarrier.fresh()
+                .waitSeconds(1.5)
+                .strafeTo(new Vector2d(0, -25))
+                .waitSeconds(0.001);
+        TrajectoryActionBuilder driveBack = moveIntoSpec1Position.fresh()
+                .strafeTo(new Vector2d(0, -35))
+                .waitSeconds(0.001);
+        TrajectoryActionBuilder push2SamplesGrabSpec = driveBack.fresh()
+                .strafeTo(new Vector2d(43, -35))
+                .strafeTo(new Vector2d(43, -10))
+                .splineTo(new Vector2d(53, -10), Math.toRadians(270))
+                .strafeTo(new Vector2d(53, -57))
+                .strafeTo(new Vector2d(53, -10))
+                .splineToConstantHeading(new Vector2d(61,-58), Math.toRadians(270))
+                .strafeTo(new Vector2d(58,-63.5))
+                .waitSeconds(0.001);
+        TrajectoryActionBuilder goToSubSecondSpec = push2SamplesGrabSpec.fresh()
+                .waitSeconds(0.5)
+                .strafeTo(new Vector2d(53, -45)) //strafe up field
+                .strafeToLinearHeading(new Vector2d(4,-45), Math.toRadians(270)) //change heading
+                .strafeTo(new Vector2d(4, -25))
+                .waitSeconds(0.001);
+        TrajectoryActionBuilder goToZoneThirdSpec = goToSubSecondSpec.fresh()
+                .waitSeconds(0.001)
+                .strafeTo(new Vector2d(4,-35))
+                .strafeToLinearHeading(new Vector2d(40,-53), Math.toRadians(90))
+                .strafeTo(new Vector2d(58,-63.5))
+                .waitSeconds(0.001);
+        TrajectoryActionBuilder goToSubThirdSpec = goToZoneThirdSpec.fresh()
+                .waitSeconds(0.5)
+                .strafeTo(new Vector2d(58, -45)) //strafe up field
+                .strafeToLinearHeading(new Vector2d(-4,-45), Math.toRadians(270)) //change heading
+                .strafeTo(new Vector2d(-4, -25))
+                .waitSeconds(0.001);
+        TrajectoryActionBuilder goBackAndPark = goToSubThirdSpec.fresh()
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(-4, -45))
+                .strafeToLinearHeading(new Vector2d(52,-59), Math.toRadians(90))
+                .waitSeconds(0.001);
+
+        Action moveAwayFromBarrierAction = moveAwayFromBarrier.build();
+        Action moveIntoSpec1PositionAction = moveIntoSpec1Position.build();
+        Action driveBackAction = driveBack.build();
+        Action push2SamplesGrabSpecAction = push2SamplesGrabSpec.build();
+        Action goToSubSecondSpecAction = goToSubSecondSpec.build();
+        Action goToZoneThirdSpecAction = goToZoneThirdSpec.build();
+        Action goToSubThirdSpecAction = goToSubThirdSpec.build();
+        Action goBackAndParkAction = goBackAndPark.build();
+
+        Action autoSequence = new SequentialAction(
+                moveAwayFromBarrierAction,
+                moveIntoSpec1PositionAction,
+                driveBackAction,
+                push2SamplesGrabSpecAction,
+                goToSubSecondSpecAction,
+                goToZoneThirdSpecAction,
+                goToSubThirdSpecAction,
+                goBackAndParkAction
+        );
+
+        myBot.runAction(autoSequence);
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
