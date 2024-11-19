d3a50eb2fe0617ee4cbc7f6ca6377dd8834509af
s0hum <sohum.p.gupta@gmail.com>
2024-11-18 16:03:49 -0800

    made meepmeep1 up to date!

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeep1.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeep1.java
@@ -22,34 +22,35 @@ public class MeepMeep1 {
         Pose2d beginPose = new Pose2d(13, -61.5, Math.toRadians(270));
 
         TrajectoryActionBuilder moveAwayFromBarrier = myBot.getDrive().actionBuilder(beginPose)
-                .strafeTo(new Vector2d(13, -50))
+                .strafeTo(new Vector2d(13, -50)) //move forward to let arm go back
                 .waitSeconds(0.001);
         TrajectoryActionBuilder moveIntoSpec1Position = moveAwayFromBarrier.fresh()
-                .strafeTo(new Vector2d(0, -28))
+                .waitSeconds(1.5) // wait for slides to go up
+                .strafeTo(new Vector2d(0, -25)) // go to sub to clip spec
                 .waitSeconds(0.001);
         TrajectoryActionBuilder driveBack = moveIntoSpec1Position.fresh()
-                .strafeTo(new Vector2d(0, -35))
+                .strafeTo(new Vector2d(0, -35)) // drive back from the sub to push sample
                 .waitSeconds(0.001);
         TrajectoryActionBuilder pushSampleGrabSpec = driveBack.fresh()
-                .splineToLinearHeading(new Pose2d(35, -35, Math.toRadians(180)), Math.toRadians(0))
-                .splineToLinearHeading(new Pose2d(55, -10, Math.toRadians(90)), Math.toRadians(0))
-                .strafeTo(new Vector2d(55, -51))
-                .strafeTo(new Vector2d(55, -57))
-                .strafeTo(new Vector2d(55, -45))
-                .waitSeconds(2)
-                .strafeTo(new Vector2d(55, -61.5))
+                .strafeTo(new Vector2d(43, -35)) // go to the right
+                .strafeTo(new Vector2d(43, -10)) // go up field
+                .splineTo(new Vector2d(53, -10), Math.toRadians(270)) //spline to push sample (turns 180 NOT relative)
+                .strafeTo(new Vector2d(53, -57)) //push spec into player person zone
+                .strafeTo(new Vector2d(53, -45)) //come out to let player person clip spec on wall
+                .strafeTo(new Vector2d(53, -63.7)) //go in to zone again to grab spec
                 .waitSeconds(0.001);
         TrajectoryActionBuilder goToSubSecondSpec = pushSampleGrabSpec.fresh()
                 .waitSeconds(0.5)
-                .strafeTo(new Vector2d(55, -45))
-                .splineToLinearHeading(new Pose2d(4, -45, Math.toRadians(270)), Math.toRadians(0))
-                .strafeTo(new Vector2d(4, -28))
+                .strafeTo(new Vector2d(53, -45)) //strafe up field
+                .strafeToLinearHeading(new Vector2d(4,-45),Math.toRadians(270)) //change heading
+                .strafeTo(new Vector2d(4, -25))
                 .waitSeconds(0.001);
         TrajectoryActionBuilder goBackAndPark = goToSubSecondSpec.fresh()
                 .waitSeconds(1)
                 .strafeTo(new Vector2d(4, -45))
-                .splineTo(new Vector2d(47, -47), Math.toRadians(90))
-                .strafeTo(new Vector2d(47, -58))
+                .strafeToLinearHeading(new Vector2d(47,-45), Math.toRadians(90))
+                .strafeTo(new Vector2d(47, -45))
+                .strafeTo(new Vector2d(47, -54))
                 .waitSeconds(0.001);
 
         Action moveAwayFromBarrierAction = moveAwayFromBarrier.build();
