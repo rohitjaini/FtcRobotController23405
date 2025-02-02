ebfe6d0597b38d545d284378d740346e44aa83da
oobeegamer <ritvikkr@outlook.com>
2025-02-02 15:14:58 -0800

    Implemented OpenCVTesting class to match VisionProcessor interface. Added method signatures and parameter types.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/openCVTesting.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import android.graphics.Canvas;
+
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.core.Mat;
+
+public class openCVTesting implements VisionProcessor {
+
+    @Override
+    public void init(int width, int height, CameraCalibration calibration) {
+        // Initialization code here
+    }
+
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+        // Frame processing code here
+        return null;
+    }
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+        // Drawing code here
+    }
+}
