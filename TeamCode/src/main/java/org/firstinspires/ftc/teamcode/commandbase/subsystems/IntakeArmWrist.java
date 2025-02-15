eee8038332b0f5224e40dd2d17d8412bf8fe6768
s0hum <sohum.p.gupta@gmail.com>
2025-02-15 08:32:57 -0800

    got started on intakeArm subsystem, updated PIDControllerWrapper to hopefully implement custom arm feedforward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/subsystems/IntakeArmWrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/subsystems/IntakeArmWrist.java
@@ -1,6 +1,33 @@
 package org.firstinspires.ftc.teamcode.commandbase.subsystems;
 
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.rowanmcalpin.nextftc.core.Subsystem;
+import com.rowanmcalpin.nextftc.ftc.OpModeData;
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx;
+
+import org.firstinspires.ftc.teamcode.tuning.PIDControllerWrapper;
 
 public class IntakeArmWrist extends Subsystem {
+
+    public static final IntakeArmWrist INSTANCE = new IntakeArmWrist();
+    private IntakeArmWrist() { }
+
+    public MotorEx intakeArmMotor;
+
+    public Servo rightWristServo;
+
+    public String intakeArmMotorName = "intakeArmMotor";
+    public String rightWristServoName = "rightWristServo";
+
+    public PIDControllerWrapper intakeArmController = new PIDControllerWrapper(new PIDController(0,0,0), 0,10);
+
+
+
+
+    @Override
+    public void initialize() {
+        rightWristServo = OpModeData.INSTANCE.getHardwareMap().get(Servo.class, rightWristServoName);
+        intakeArmMotor = new MotorEx(intakeArmMotorName);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/PIDControllerWrapper.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/PIDControllerWrapper.kt
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.tuning
 
 import com.arcrobotics.ftclib.controller.PIDController
 import com.rowanmcalpin.nextftc.core.control.controllers.Controller
+import kotlin.math.cos
 
 class PIDControllerWrapper(
     val ftcLibController: PIDController,
@@ -16,6 +17,19 @@ class PIDControllerWrapper(
 
     override fun calculate(reference: Double): Double {
         return ftcLibController.calculate(reference) + ff
+    }
+
+    fun calculateArmFeedForward(
+        targetPosition: Double,
+        ticksInDegrees: Double,
+        initialPositionForFF: Double,
+        f: Double
+    ): Double {
+        return cos(
+            Math.toRadians(targetPosition / ticksInDegrees) - Math.toRadians(
+                initialPositionForFF
+            ) + Math.toRadians(90.0)
+        ) * f
     }
 
     override fun reset() {
